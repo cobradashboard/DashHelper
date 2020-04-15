c24aace035628917f898ebc229202354096e4030
ktmshyamal <ktmshyamal@gmail.com>
2020-04-15 22:06:32 +0530

    oncreat & onclick

--- app/src/main/java/com/example/dashhelper/MainActivity.java
+++ app/src/main/java/com/example/dashhelper/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.dashhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.cardview.widget.CardView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private CardView work,holiday,timetable,community;
+    private Button login,logout;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        work = (CardView)findViewById(R.id.crdId1);
+        work.setOnClickListener(this);
+
+        }
+
+    @Override
+    public void onClick(View view) {
+        intent i;
+
+        Switch(view.getId()){
+            case R.id.crdId1 : i=new Intent(this,workouthome.class);startActivity(i);break;
+
+            default:break;
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/dashhelper/donned_workouts.java
@@ -0,0 +1,14 @@
+package com.example.dashhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donned_workouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donned_workouts);
+    }
+}
