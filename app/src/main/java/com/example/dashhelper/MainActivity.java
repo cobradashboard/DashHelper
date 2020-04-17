0fce6a81bbd2dd57d34806b9f8a2a448bb8c9723
ktmshyamal <ktmshyamal@gmail.com>
2020-04-17 23:59:59 +0530

    oncreat & onclick

--- app/src/main/java/com/example/dashhelper/MainActivity.java
+++ app/src/main/java/com/example/dashhelper/MainActivity.java
@@ -1,42 +1,47 @@
 package com.example.dashhelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import androidx.cardview.widget.CardView;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+
 
     private CardView work,holiday,timetable,community;
     private Button login,logout;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        work = (CardView)findViewById(R.id.crdId1);
+        work=(CardView)findViewById(R.id.cardId1);
+
         work.setOnClickListener(this);
 
-        }
+    }
 
     @Override
     public void onClick(View view) {
-        intent i;
+        Intent i;
 
-        Switch(view.getId()){
-            case R.id.crdId1 : i=new Intent(this,workouthome.class);startActivity(i);break;
+        switch(view.getId())
+        {
+            case R.id.cardId1 : i=new Intent(this,workouthome.class);startActivity(i);break;
 
-            default:break;
+
+
+
         }
+
     }
 
 
-    private class intent {
-    }
 }
