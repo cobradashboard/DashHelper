cca8b95422ddd32746d244857cfe473c8442ab8d
lahiruLiyanage <lahiruliyanage001@gmail.com>
2020-04-21 23:59:44 +0530

    change UI backgrounds

--- app/src/main/java/com/example/dashhelper/view_result.java
+++ app/src/main/java/com/example/dashhelper/view_result.java
@@ -2,7 +2,10 @@ package com.example.dashhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class view_result extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class view_result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_result);
+    }
+    public  void study (View view){
+        Intent studyintent = new Intent(Intent.ACTION_VIEW, Uri.EMPTY.parse("http://study.sliit.lk/"));
+        startActivity(studyintent);
     }
 }
