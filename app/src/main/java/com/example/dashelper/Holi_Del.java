86513d26f30afe37f8f8e3eedee256c6808d4d68
Tharindu Dilshan Samarakoon <54529483+TharinduSamarakoon@users.noreply.github.com>
2020-05-05 02:08:03 +0530

    Update Holi_Del.java

--- app/src/main/java/com/example/dashelper/Holi_Del.java
+++ app/src/main/java/com/example/dashelper/Holi_Del.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 public class Holi_Del extends Application {
     public ArrayList<String> garrList= new ArrayList<>();
     public ArrayAdapter<String> garrAdp;
+    public String gvalue_Hid;
     public String gvalue_Hname;
-    public String gvalue_Hdiscription;
+    
 
     public String getGvalue_Hname() {
         return gvalue_Hname;
@@ -17,11 +18,11 @@ public class Holi_Del extends Application {
     public void setGvalue_Hname(String gvalue_hname) {
         this.gvalue_Hname=gvalue_hname;
     }
-    public String getGvalue_Hdiscription() {
-        return gvalue_Hdiscription;
+    public String getGvalue_Hid() {
+        return gvalue_Hid;
     }
-    public void setGvalue_Hdiscription(String gvalue_hdiscription) {
-        this.gvalue_Hdiscription=gvalue_hdiscription;
+    public void setGvalue_Hid(String gvalue_hid) {
+        this.gvalue_Hid=gvalue_hid;
     }
 }
 //
